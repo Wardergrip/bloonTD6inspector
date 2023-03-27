48cb90362f5d99089d35ec4ed08ce5f19bfddc75
Wardergrip <42802496+ReiMessely@users.noreply.github.com>
2023-03-27 15:27:37 +0200

    feat: full caps converter

---
+++ BloonsTD6Inspector/View/Converters/StringToFullCapsConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace BloonsTD6Inspector.View.Converters
+{
+    public class StringToFullCapsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((string)value).ToUpper();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

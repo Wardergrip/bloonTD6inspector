b2b7949282753cfbb69c99cdf10c049a1a2fb6f2
Wardergrip <42802496+ReiMessely@users.noreply.github.com>
2023-03-27 10:11:32 +0200

    feat: GmaeObject and initial detailPage

---
+++ BloonsTD6Inspector/ViewModel/DetailedPageVM.cs
@@ -0,0 +1,26 @@
+﻿using BloonsTD6Inspector.Model;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloonsTD6Inspector.ViewModel
+{
+    public class DetailedPageVM : ObservableObject
+    {
+        public GameObject InspectedObject { get; set; }
+
+        public DetailedPageVM() 
+        {
+            InspectedObject = new GameObject()
+            {
+                Type = "hero",
+                Name = "quincy",
+                Description = "Proud, strong and intelligent, Quincy uses his bow to perform feats of amazing skill.",
+                Id = "quincy"
+            };
+        }
+    }
+}
--- BloonsTD6Inspector/ViewModel/MainViewModel.cs
+++ BloonsTD6Inspector/ViewModel/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using BloonsTD6Inspector.View;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
@@ -13,44 +14,45 @@ namespace BloonsTD6Inspector.ViewModel
     {
         public RelayCommand SwitchPageCommand { get; private set; }
         public string CommandText { get; private set; }
-        //public OverviewPage MainPage { get; private set; } = new OverviewPage();
-        //public DetailedPage PokePage { get; private set; } = new DetailedPage();
+        public OverviewPage MainPage { get; private set; } = new OverviewPage();
+        public DetailPage DetailedPage { get; private set; } = new DetailPage();
         public Page CurrentPage { get; private set; }
-
+        
         public MainViewModel()
         {
             //CurrentPage = MainPage;
+            CurrentPage = DetailedPage;
             SwitchPageCommand = new RelayCommand(SwitchPage);
             CommandText = "SHOW DETAILS";
         }
 
         public void SwitchPage()
         {
-            //check the current visisble page type
-            //if (CurrentPage is OverviewPage)
-            //{
-            //    // Get the selected pokemon
-            //    Pokemon selectedPokemon = (MainPage.DataContext as OverviewVM).SelectedPokemon;
-            //    if (selectedPokemon == null) return;
+            // check the current visisble page type
+            if (CurrentPage is OverviewPage)
+            {
+                // Get the selected pokemon
+                //Pokemon selectedPokemon = (MainPage.DataContext as OverviewVM).SelectedPokemon;
+                //if (selectedPokemon == null) return;
 
-            //    //TODO: Set the currentPokemon of the DetailPageVM to the selected pokemon
-            //    //Hint: use the same principle as above to access the datacontext of the detailpage
-            //    DetailPageVM dpVM = (PokePage.DataContext as DetailPageVM);
-            //    dpVM.CurrentPokemon = selectedPokemon;
+                //TODO: Set the currentPokemon of the DetailPageVM to the selected pokemon
+                //Hint: use the same principle as above to access the datacontext of the detailpage
+                //DetailPageVM dpVM = (PokePage.DataContext as DetailPageVM);
+                //dpVM.CurrentPokemon = selectedPokemon;
 
-            //    // Set the current page TODO: notify the view of the change!
-            //    CurrentPage = PokePage;
-            //    CommandText = "GO BACK";
-            //    OnPropertyChanged(nameof(CommandText));
-            //    OnPropertyChanged(nameof(CurrentPage));
-            //}
-            //else
-            //{
-            //    CurrentPage = MainPage;
-            //    CommandText = "SHOW DETAILS";
-            //    OnPropertyChanged(nameof(CommandText));
-            //    OnPropertyChanged(nameof(CurrentPage));
-            //}
+                // Set the current page TODO: notify the view of the change!
+                CurrentPage = DetailedPage;
+                CommandText = "GO BACK";
+                OnPropertyChanged(nameof(CommandText));
+                OnPropertyChanged(nameof(CurrentPage));
+            }
+            else
+            {
+                CurrentPage = MainPage;
+                CommandText = "SHOW DETAILS";
+                OnPropertyChanged(nameof(CommandText));
+                OnPropertyChanged(nameof(CurrentPage));
+            }
         }
     }
 }
